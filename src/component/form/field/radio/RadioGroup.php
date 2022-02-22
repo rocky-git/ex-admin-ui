c75b5dc9d96212207590dc542497532d37b60caf
maple <598236556@qq.com>
2022-02-23 00:13:29 +0800

    分组下拉框，单选框样式切换

--- src/component/form/field/radio/RadioGroup.php
+++ src/component/form/field/radio/RadioGroup.php
@@ -12,7 +12,6 @@ use ExAdmin\ui\component\form\Field;
  * @method $this buttonStyle(string $style = 'outline') RadioButton 的风格样式，目前有描边和填色两种风格					outline | solid
  * @method $this disabled(bool $disabled = false) 禁选所有子单选器														boolean
  * @method $this name(string $name) RadioGroup 下所有 input[type="radio"] 的 name 属性									string
- * @method $this optionType(string $type = 'default') 用于设置 Radio options 类型										default | button
  * @method $this size(string $size = 'default') 大小，只对按钮样式生效														large | default | small
  * @package ExAdmin\ui\component\form\field
  */
@@ -25,13 +24,21 @@ class RadioGroup extends Field
 	protected $name = 'ARadioGroup';
 
     /**
+     * 单选框类型
+     * @var bool
+     */
+    protected $buttonType = false;
+
+    /**
      * 设置选项
-     * @param array $data
-     * @param array $disabledArr
+     * @param array $data 数据源 [key => value] 的形式
+     * @param array $disabledArr 禁选的id
+     * @param bool $buttonType false 默认 true 按钮
      * @return $this
      */
-    public function options(array $data, $disabledArr = [])
+    public function options(array $data, $disabledArr = [], bool $buttonType = false)
     {
+        $this->buttonType = $buttonType;
         $options = [];
         foreach($data as $id => $value) {
             $disabled = false;
@@ -44,13 +51,26 @@ class RadioGroup extends Field
                 'slotDefault' => $value,
             ];
         }
-
-        $radioOption = Radio::create()
+        $radioOption = $this->getButton()
             ->map($options)
             ->mapAttr('value')
             ->mapAttr('disabled')
             ->mapAttr('slotDefault');
         $this->content($radioOption);
         return $this;
+    }
+
+    /**
+     * 获取按钮类型 radio 默认单选 RadioButton 单选按钮
+     * @return Radio|RadioButton
+     */
+    protected function getButton()
+    {
+        if ($this->buttonType) {
+            $button = RadioButton::create();
+        } else {
+            $button = Radio::create();
+        }
+        return $button;
     }
 }
--- src/component/form/field/select/Select.php
+++ src/component/form/field/select/Select.php
@@ -10,42 +10,42 @@ use ExAdmin\ui\component\form\Field;
  * 选择器
  * Class Select
  * @link   https://next.antdv.com/components/select-cn 选择器组件
- * @method $this allowClear(bool $clear = false) 支持清除																boolean
- * @method $this autoClearSearchValue(bool $search = true) 是否在选中项后清空搜索框，只在 mode 为 multiple 或 tags 时有效。	boolean
- * @method $this autofocus(bool $focus = false) 默认获取焦点																boolean
- * @method $this bordered(bool $bordered = true) 是否有边框																boolean
- * @method $this defaultActiveFirstOption(bool $active = true) 是否默认高亮第一个选项。									boolean
- * @method $this disabled(bool $disabled = false) 是否禁用																boolean
- * @method $this dropdownClassName(string $name) 下拉菜单的 className 属性												string
+ * @method $this allowClear(bool $clear = false) 支持清除                                                                boolean
+ * @method $this autoClearSearchValue(bool $search = true) 是否在选中项后清空搜索框，只在 mode 为 multiple 或 tags 时有效。    boolean
+ * @method $this autofocus(bool $focus = false) 默认获取焦点                                                                boolean
+ * @method $this bordered(bool $bordered = true) 是否有边框                                                                boolean
+ * @method $this defaultActiveFirstOption(bool $active = true) 是否默认高亮第一个选项。                                    boolean
+ * @method $this disabled(bool $disabled = false) 是否禁用                                                                boolean
+ * @method $this dropdownClassName(string $name) 下拉菜单的 className 属性                                                string
  * @method $this dropdownMatchSelectWidth(mixed $match = true) 下拉菜单和选择器同宽。默认将设置 min-width，
- * 																当值小于选择框宽度时会被忽略。false 时会关闭虚拟滚动          boolean | number
- * @method $this dropdownStyle(mixed $style) 下拉菜单的 style 属性														object
- * @method $this dropdownMenuStyle(mixed $style) dropdown 菜单自定义样式													object
+ *                                                                当值小于选择框宽度时会被忽略。false 时会关闭虚拟滚动          boolean | number
+ * @method $this dropdownStyle(mixed $style) 下拉菜单的 style 属性                                                        object
+ * @method $this dropdownMenuStyle(mixed $style) dropdown 菜单自定义样式                                                    object
  * @method $this filterOption(mixed $filter = true) 是否根据输入项进行筛选。当其为一个函数时，会接收 inputValue option
- * 											两个参数，当 option 符合筛选条件时，应返回 true，反之则返回 false。               boolean or function(inputValue, option)
- * @method $this firstActiveValue(mixed $active) 默认高亮的选项															string|string[]
+ *                                            两个参数，当 option 符合筛选条件时，应返回 true，反之则返回 false。               boolean or function(inputValue, option)
+ * @method $this firstActiveValue(mixed $active) 默认高亮的选项                                                            string|string[]
  * @method $this labelInValue(bool $labelValue = false) 是否把每个选项的 label 包装到 value 中，会把 Select 的 value 类型从
- * 													     string 变为 {key: string, label: vNodes} 的格式                 boolean
- * @method $this maxTagCount(int $num) 最多显示多少个 tag																	number
- * @method $this maxTagTextLength(int $length) 最大显示的 tag 文本长度													number
- * @method $this mode(string $mode) 设置 Select 的模式为多选或标签															'multiple' | 'tags' | 'combobox'
- * @method $this notFoundContent(mixed $content = 'Not Found') 当下拉列表为空时显示的内容									string|slot
- * @method $this optionFilterProp(string $value) 搜索时过滤对应的 option 属性，不支持 children								string
+ *                                                         string 变为 {key: string, label: vNodes} 的格式                 boolean
+ * @method $this maxTagCount(int $num) 最多显示多少个 tag                                                                    number
+ * @method $this maxTagTextLength(int $length) 最大显示的 tag 文本长度                                                    number
+ * @method $this mode(string $mode) 设置 Select 的模式为多选或标签                                                            'multiple' | 'tags' | 'combobox'
+ * @method $this notFoundContent(mixed $content = 'Not Found') 当下拉列表为空时显示的内容                                    string|slot
+ * @method $this optionFilterProp(string $value) 搜索时过滤对应的 option 属性，不支持 children                                string
  * @method $this optionLabelProp(string $label) 回填到选择框的 Option 的属性值，默认是 Option 的子元素。比如在子元素
- * 														需要高亮效果时，此值可以设为 value。                                string  children | label(设置 options 时)
- * @method $this placeholder(string $placeholder) 选择框默认文字															string|slot
- * @method $this showSearch(bool $search = false) 使单选模式可搜索														boolean
- * @method $this showArrow(bool $show = true) 是否显示下拉小箭头															boolean
- * @method $this size(string $size = 'default') 选择框大小，可选 large small default										string
- * @method $this tokenSeparators(array $separators) 在 tags 和 multiple 模式下自动分词的分隔符								string[]
- * @method $this value(mixed $value) 指定当前选中的条目																	string|string[]|number|number[]
- * @method $this defaultOpen(bool $open) 是否默认展开下拉菜单																boolean
- * @method $this open(bool $open) 是否展开下拉菜单																		boolean
+ *                                                        需要高亮效果时，此值可以设为 value。                                string  children | label(设置 options 时)
+ * @method $this placeholder(string $placeholder) 选择框默认文字                                                            string|slot
+ * @method $this showSearch(bool $search = false) 使单选模式可搜索                                                        boolean
+ * @method $this showArrow(bool $show = true) 是否显示下拉小箭头                                                            boolean
+ * @method $this size(string $size = 'default') 选择框大小，可选 large small default                                        string
+ * @method $this tokenSeparators(array $separators) 在 tags 和 multiple 模式下自动分词的分隔符                                string[]
+ * @method $this value(mixed $value) 指定当前选中的条目                                                                    string|string[]|number|number[]
+ * @method $this defaultOpen(bool $open) 是否默认展开下拉菜单                                                                boolean
+ * @method $this open(bool $open) 是否展开下拉菜单                                                                        boolean
  * @package ExAdmin\ui\component\form\field
  */
 class Select extends Field
 {
-	/**
+    /**
      * 插槽
      * @var string[]
      */
@@ -57,8 +57,14 @@ class Select extends Field
      * 组件名称
      * @var string
      */
-	protected $name = 'ASelect';
-    
+    protected $name = 'ASelect';
+
+    /**
+     * 禁用的选项
+     * @var array
+     */
+    protected $disabledData = [];
+
     public function __construct($field = null, $value = '')
     {
         $this->allowClear();
@@ -71,11 +77,12 @@ class Select extends Field
      * 多选
      * @return $this
      */
-    public function multiple(){
+    public function multiple()
+    {
         $value = $this->getbindAttrValue('value');
-        if(!is_array($value)){
+        if (!is_array($value)) {
             $field = $this->bindAttr('value');
-            $this->bind($field,[]);
+            $this->bind($field, []);
         }
         return $this->mode('multiple');
     }
@@ -86,28 +93,93 @@ class Select extends Field
      * @param array $disable 禁用选项数据
      * @return Select
      */
-	public function options(array $data, array $disable = []){
+    public function options(array $data, array $disable = [])
+    {
         $options = [];
+        $this->disabledData = $disabled;
         foreach ($data as $id => $title) {
             $disabled = false;
-            if (in_array($id, $disable)) {
+            if (in_array($id, $this->disabledData)) {
                 $disabled = true;
             }
             $options[] = [
-                'value' => $id,
-                'title' => $title,
-                'disabled' => $disabled,
+                'value'       => $id,
+                'title'       => $title,
+                'disabled'    => $disabled,
                 'slotDefault' => $title,
             ];
         }
 
         $selectOption = SelectOption::create()
-            ->map($options)
-            ->mapAttr('value')
-            ->mapAttr('title')
-            ->mapAttr('disabled')
-            ->mapAttr('slotDefault');
+                                    ->map($options)
+                                    ->mapAttr('value')
+                                    ->mapAttr('title')
+                                    ->mapAttr('disabled')
+                                    ->mapAttr('slotDefault');
         $this->content($selectOption);
+        return $this;
+    }
+
+    /**
+     * 分组下拉框
+     * @param array $data 数组源
+     * @param array $disabledArr 禁用的选项
+     * @param string $name 关联的字段
+     * @param string $label 名称的字段
+     * @param string $id 主键的字段
+     * @return $this
+     */
+    public function groupOptions(array $data, array $disabledArr = [], $name = 'options', $label = 'label', $id = 'id')
+    {
+        /* 格式
+         $data = [
+            [
+                'label' => '第一个分组',
+                'id' => 2,
+                'options' => [
+                    [
+                        'label' => '第一个标签',
+                        'id' => 1
+                    ]
+                ]
+            ],
+            [
+                'label' => '第二个分组',
+                'id' => 2,
+                'options' => [
+                    [
+                        'label' => '第二个标签',
+                        'id' => 2
+                    ]
+                ]
+            ]
+         ];
+        */
+        $this->disabledData = $disabledArr;
+        foreach ($data as $key => $option) {
+            $disabled = false;
+            if (in_array($option[$id], $this->disabledData)) {
+                $disabled = true;
+            }
+            $selectGroup = SelectGroup::create()
+                                      ->attr('label', $option[$label])
+                                      ->attr('slotDefault', $option[$label])
+                                      ->attr('disabled', $disabled);
+            foreach ($option[$name] as $item) {
+                $disabled = false;
+                if (in_array($item[$id], $this->disabledData)) {
+                    $disabled = true;
+                }
+                $selectGroup->content(
+                    SelectOption::create()
+                                ->attr('title', $item[$label])
+                                ->attr('slotDefault', $item[$label])
+                                ->attr('disabled', $disabled)
+                                ->attr('value', $item[$id])
+                );
+            }
+            $this->content($selectGroup);
+        }
         return $this;
     }
 }
