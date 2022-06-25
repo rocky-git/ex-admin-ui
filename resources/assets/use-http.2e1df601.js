34bccb4137b5bcfffdeca0dff8ede634d0083541
rocky-git <631670747@qq.com>
2022-06-25 20:57:33 +0800

    新增token，版本切换1.0.1

--- resources/assets/use-http.2e1df601.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./index.517d8f54.js";import"./vue.303f3630.js";import{r as a}from"./@vue.cfb5b4bf.js";const l=function(){const t=a(!1);return{loading:t,http:function(r){return new Promise((n,o)=>{t.value=!0,s(r).then(e=>{n(e)}).catch(e=>{o(e)}).finally(()=>{t.value=!1})})}}};export{l as u};
