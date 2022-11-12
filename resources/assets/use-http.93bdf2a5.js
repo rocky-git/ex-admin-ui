630bfe15060a3f65db1b08eb9aebd1dc8048d678
rocky <631670747@qq.com>
2022-11-12 16:42:05 +0800

    ui 适配手机端

---
+++ resources/assets/use-http.93bdf2a5.js
@@ -0,0 +1,1 @@
+import{s}from"./index.d3154ee4.js";import"./vue.db505ee4.js";import{r as a}from"./@vue.cb43a243.js";const l=function(){const t=a(!1);return{loading:t,http:function(r){return new Promise((n,o)=>{t.value=!0,s(r).then(e=>{n(e)}).catch(e=>{o(e)}).finally(()=>{t.value=!1})})}}};export{l as u};
